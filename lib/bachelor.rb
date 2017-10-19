46803f833c3584056c277a5e442f7aaa867a74da
lierica <ericaaa.li@gmail.com>
2017-10-18 21:18:18 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data.each do |season_number, contestants|
+        if season_number == season
+            contestants.each do |attribute|
+            if attribute["status"] == "Winner"
+                winner_name = attribute["name"].split(" ")
+                return winner_name[0]
+                end
+            end
+        end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    data.each do |season_number, contestants|
+        contestants.each do |attribute|
+            if attribute["occupation"] == occupation
+                return attribute["name"]
+            end
+        end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+    hometown_counter = 0
+    data.each do |season_number, contestants|
+        contestants.each do |attribute|
+            if attribute["hometown"] == hometown
+                hometown_counter += 1
+            end
+        end
+    end
+    hometown_counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season_number, contestants|
+        contestants.each do |attribute|
+            if attribute["hometown"] == hometown
+                return attribute["occupation"]
+            end
+        end
+    end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+    age_array = []
+    data.each do |season_number, contestants|
+        if season_number == season
+            contestants.each do |attribute|
+                age_array << attribute["age"].to_f
+            end
+        end
+    end
+    (age_array.reduce(:+)/age_array.length).round
 end
